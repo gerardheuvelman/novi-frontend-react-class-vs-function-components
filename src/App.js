a28c094b0a62436bbc99f5a2dd910ebdce4f691c
gerardheuvelman <gerardheuvelman@gmail.com>
2022-10-13 11:55:51 +0200

    opdracht af

--- src/App.js
+++ src/App.js
@@ -2,10 +2,29 @@ import React from 'react';
 import './App.css';
 
 function App() {
+
+  const [state, setState] = React.useState({isLightOn : false})
+
+  function handleClick (evt) {
+      // console.log("handleclick!");
+      // console.log(state.isLightOn);
+      // const value = evt.target.type === "checkbox" ? evt.target.checked : evt.target.value;
+      setState({
+          ...state,
+          [evt.target.name]: !state.isLightOn
+      });
+  }
+
   return (
-    <div>
-      Schrijf hier jouw functie-component!
-    </div>
+    <>
+      <button
+          type="button"
+          name="isLightOn"
+          onClick={handleClick}
+      >
+          {state.isLightOn? "ON" : "OFF"}
+      </button>
+    </>
   );
 }
 
